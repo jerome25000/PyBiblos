84b0113ae60491b1cb34b5682adcee45bd0843a4
jerome25000 <--global>
2019-12-30 18:24:12 +0100

    new modif

--- tavern.sh
+++ tavern.sh
@@ -1,6 +1,6 @@
 python3 serv.py test.db --test &
-py.test test_authors.tavern.yaml
-py.test test_pagination.tavern.yaml
-py.test test_books.tavern.yaml
+python3 -m pytest test_authors.tavern.yaml
+python3 -m pytest test_pagination.tavern.yaml
+python3 -m pytest test_books.tavern.yaml
 #kill process python run before, $$ is pid of this script, pkill -P : kill all processes created by this script!
 pkill -P $$
