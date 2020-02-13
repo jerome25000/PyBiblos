bfaff43b6adbff1e408a3bb2d47cc37dbcc34122
jcamus <jerome.camus@flowbird.group>
2020-02-13 14:12:59 +0100

    Change CSS for pagination

--- static/book.js
+++ static/book.js
@@ -1,25 +1,35 @@
+Vue.component('paginate', VuejsPaginate)
+
 var app = new Vue({ 
     el: '#app',
     data: {
         message: 'Hello Vue!!!',
         authors : null,
         books : null,
-        book1 : null
+        book1 : null,
+        pages : 15
+    },
+    methods: {
+      click: function(page) {
+        console.log(page)
+      }
     },
     mounted: function() {
       axios
-       .get('http://192.168.1.7/authors')
+       .get('http://localhost/authors')
        .then(response => (this.authors = response.data.data))
-       .catch(error => console.log(error))
+       .catch(error => console.log(error));
 
-       axios
-       .get('http://192.168.1.7/books')
-       .then(response => (this.books = response.data.data))
-       .catch(error => console.log(error))
+       // axios
+       // .get('http://localhost/books')
+       // .then(response => (this.books = response.data.data))
+       // .catch(error => console.log(error))
 
-       axios
-       .get('http://192.168.1.7/books/1')
-       .then(response => (this.book1 = response))
-       .catch(error => console.log(error))
+       // axios
+       // .get('http://localhost/books/1')
+       // .then(response => (this.book1 = response))
+       // .catch(error => console.log(error))
     }
 });
+
+
